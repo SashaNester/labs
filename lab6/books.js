c91a4125cd6262540d849d5c26e72e4c43ad65ab
Nesterovasasha <a.nesterova3@g.nsu.ru>
2025-05-29 22:43:57 +0300

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author:'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: '210',
+        bestseller: true,
+        available: false
+    },
+    {
+        author:'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: '250',
+        bestseller: false,
+        available: true
+    },
+    {
+        author:'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: '300',
+        bestseller: false,
+        available: true
+    },
+    {
+        author:'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: '440',
+        bestseller: true,
+        available: true
+    },
+    {
+        author:'Уильям Шекспир',
+        name: 'Гамлет',
+        publicationYear: 1603,
+        price: '200',
+        bestseller: false,
+        available: true
+    },
+    {
+        author:'Джеймс Джойс',
+        name: 'Улисс',
+        publicationYear: 1922,
+        price: '350',
+        bestseller: true,
+        available: true
+    },
+    {
+        author:'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: '300',
+        bestseller: true,
+        available: false
+    },
+    {
+        author:'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: '270',
+        bestseller: true,
+        available: true
+    },
+    {
+        author:'Владимир Набоков',
+        name: 'Лолита',
+        publicationYear: 1955,
+        price: '320',
+        bestseller: false,
+        available: false
+    },
+    {
+        author:'Пауло Коэльо',
+        name: 'Алхимик',
+        publicationYear: 1988,
+        price: '300',
+        bestseller: false,
+        available: false
+    }
+]
